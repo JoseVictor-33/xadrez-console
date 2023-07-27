e1099cdbc63313fcf2236eaa248af5523b82a224
Jose Victor <joser_hto@outlook.com>
2023-07-27 20:26:00 -0300

    Comecando a implementar metodo que imprime o tabuleiro

--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -10,5 +10,11 @@
             this.colunas = coluna;
             pecas = new Peca[linhas, colunas];
         }
+
+        public Peca peca (int linha, int coluna) {
+
+            return pecas[linha, coluna];
+
+        }
     }
 }
